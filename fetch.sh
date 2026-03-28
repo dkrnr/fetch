6e869d2f4131b42a03c483e5d9f0d3ce0fb96b1c
dkrnr <kernerdunith@gmail.com>
2026-03-29 00:03:43 +0530

    Added more info + ASCII art

--- fetch.sh
+++ fetch.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
 KERNEL=$(uname -r)
+USER=$(whoami)
 HOSTNAME=$(hostnamectl hostname)
 OS=$(cat /etc/os-release | grep PRETTY | cut -d '"' -f 2)
 ARCH=$(uname -m)
 
-echo -e "\nSys Info"
-echo -e "+----------------------+"
-echo -e "OS: ${OS}"
-echo -e "Architecture: ${ARCH}"
-echo -e "Kernel: ${KERNEL}"
-echo -e "Hostname: ${HOSTNAME}"
-echo -e "+----------------------+\n"
+CPU=$(grep -m1 "model name" /proc/cpuinfo | cut -d: -f2)
+TMEM=$(grep MemTotal /proc/meminfo | cut -d : -f 2)
+
+ART="
+
+		*----------------*
+		 \	/\      /
+		  \    /  \    /
+		   \  /____\  /	
+		   /  \||||/  \	
+		  /    \  /    \     
+		 /	\/	\      
+		*----------------*     
+
+"
+
+echo -e "\nloading sys info for\t\t$USER@$HOSTNAME"
+echo -e "${ART}"
+echo -e "+-------------------------------------------------+"
+echo -e "| OS: ${OS}"
+echo -e "| Architecture: ${ARCH}"
+echo -e "| Kernel: ${KERNEL}"
+echo -e "| Hostname: ${HOSTNAME}"
+echo -e "+-------------------------------------------------+"
+echo -e "| CPU:${CPU}"
+echo -e "| Total Memory:${TMEM}"
+echo -e "| Shell:${SHELL}"
+echo -e "| Terminal:${TERM}"
+echo -e "+-------------------------------------------------+\n"
